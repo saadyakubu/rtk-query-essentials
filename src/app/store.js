905ae38abdfd044ed9502acf15f790cd66cecd7e
Saad Yakubu <saadyakubu2@yahoo.com>
2024-03-21 15:26:26 +0100

    Added apiSlice for the app

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,14 @@ import { configureStore } from '@reduxjs/toolkit'
 import postsReducer from '../features/posts/postsSlice'
 import usersSlice from '../features/users/usersSlice'
 import notificationsSlice from '../features/notifications/notificationsSlice'
+import { apiSlice } from '../features/api/apiSlice'
 
 export const store = configureStore({
   reducer: {
     posts: postsReducer,
     users: usersSlice,
     notifications: notificationsSlice,
+    [apiSlice.reducerPath]: apiSlice.reducer,
   },
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(apiSlice.middleware)
 })
