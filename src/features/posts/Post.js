d741ba6560acbe75757402646f8dd7e5c64b7a9f
Saad Yakubu <saadyakubu2@yahoo.com>
2024-03-22 11:47:20 +0100

    single post endpoint

--- src/features/posts/Post.js
+++ src/features/posts/Post.js
@@ -5,31 +5,44 @@ import { PostAuthor } from './PostAuthor'
 import TimeAgo from './TimeAgo'
 import { ReactionButtons } from './ReactionButtons'
 
-import { selectPostById } from './postsSlice'
+//import { selectPostById } from './postsSlice'
+
+import { Spinner } from '../../components/Spinner'
+import { useGetPostQuery } from '../api/apiSlice'
 
 
-export const Post = ({match}) => {
+export const Post = ({ match }) => {
     const { postId } = match.params
 
-    const post = useSelector(state => selectPostById(state, postId))//useSelector(state => state.posts.find(post => post.id === postId))
+    //const post = useSelector(state => selectPostById(state, postId))//useSelector(state => state.posts.find(post => post.id === postId))
+    const { data: post, isFetching, isSuccess, isError } = useGetPostQuery(postId)
 
-    if(!post){
-        return (
-            <section>
-                <h2>Post not found!</h2>
-            </section>
+    let content
+    if (isFetching) {
+        content = <Spinner text='Loading...' />
+    }
+    else if (isSuccess) {
+        content = (
+            <article>
+                <h2>{post.title}</h2>
+                <p className='post-content'>{post.content}</p>
+                <ReactionButtons post={post} />
+                <PostAuthor userId={post.user} /> &nbsp; <TimeAgo timestamp={post.date} />
+                <Link to={`/edit-post/${post.id}`} className="button">Edit Post</Link>
+            </article>
+        )
+    }
+    else if (isError) {
+        content = (
+            <article>
+                <h2>Error loading post!</h2>
+            </article>
         )
     }
 
+
+
     return (
-        <section>
-            <h2>{post.title}</h2>
-            <p className='post-content'>{post.content}</p>
-            <ReactionButtons post={post}/>
-            <PostAuthor userId={post.user}/> &nbsp; <TimeAgo timestamp={post.date} />
-            <Link to={`/edit-post/${post.id}`} className="button">Edit Post</Link>
-        </section>
+        <section>{ content }</section>
     )
 }
-
-export default Post
