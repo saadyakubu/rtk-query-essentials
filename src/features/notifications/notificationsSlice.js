897be511764a240d73b933f2747f73dbe104bd68
Saad Yakubu <saadyakubu2@yahoo.com>
2024-03-24 14:43:37 +0100

    Implementing optimistic updates - notifications

--- src/features/notifications/notificationsSlice.js
+++ src/features/notifications/notificationsSlice.js
@@ -1,6 +1,73 @@
 import React from 'react'
-import { createAsyncThunk, createSlice, createEntityAdapter } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice, createEntityAdapter, createSelector } from '@reduxjs/toolkit'
 import { client } from '../../api/client'
+
+import { forceGenerateNotifications } from '../../api/server'
+import { apiSlice } from '../api/apiSlice'
+
+export const extendedApi = apiSlice.injectEndpoints({
+    endpoints: builder => ({
+        getNotifications: builder.query({
+            query: () => '/notifications',
+            async onCacheEntryAdded(
+                arg,
+                { updateCachedData, cacheDataLoaded, cacheEntryRemoved }
+            ){
+                //create a web socket connection when the cache subscription starts
+                const ws = new WebSocket('ws://localhost')
+                try {
+                    //wait for the initial query to resolve before proceeding
+                    await cacheDataLoaded
+
+                    //when the data is received from the socket connection to the server, update query result with received msg
+                    const listener = event => {
+                        const message = JSON.parse(event.data)
+                        switch(message.type){
+                            case 'notifications': {
+                                updateCachedData(draft => {
+                                    //insert all received notifications from the websocket into the existing RTKQ cache array
+                                    draft.push(...message.payload)
+                                    draft.sort((a, b)=> b.date.localeCompare(a.date))
+                                })
+                                break
+                            }
+                            default:
+                                break
+                        }
+                    }
+
+                    ws.addEventListener('message', listener)
+                } catch {
+                    // no-op in case `cacheEntryRemoved` resolves before `cacheDataLoaded`, in which case `cacheDataLoaded` will throw
+                }
+
+                // cacheEntryRemoved will resolve when the cache subscription is no longer active
+                await cacheEntryRemoved
+                //perform cleanup steps once the `cacheEntryRemoved` promise resolves
+                ws.close()
+            }
+        })
+    })
+})
+
+export const { useGetNotificationsQuery } = extendedApi
+
+const emptyNotifications = []
+
+export const selectionNotificationsResult = extendedApi.endpoints.getNotifications.select()
+
+const selectNotificationsData = createSelector(
+    selectionNotificationsResult,
+    notificationsResult => notificationsResult?.data ?? emptyNotifications
+)
+
+export const fetchNotificationsWebsocket = () => (dispatch, getState) => {
+    const allNotifications = selectNotificationsData(getState())
+    const [ latestNotification ] = allNotifications
+    const latestTimestamp = latestNotification?.data.date ?? ''
+    //hardcode a call to the mock server to simulate a server push scenario over websockets
+    forceGenerateNotifications(latestTimestamp)
+}
 
 const notificationsAdapter = createEntityAdapter({
     sortComparer: (a, b) => b.date.localeCompare(a.date)
