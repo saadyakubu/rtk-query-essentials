744bdcea1cad7081a65eeb70fcd8f36e291a3ecd
Saad Yakubu <saadyakubu2@yahoo.com>
2024-03-24 12:00:13 +0100

    users api via code splitting

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -14,10 +14,10 @@ import { apiSlice } from "../api/apiSlice"
 // const usersAdapter = createEntityAdapter()
 // const initialState = usersAdapter.getInitialState()
 
-export const fetchUsers = createAsyncThunk(`/users/fetchUser`, async() => {
+/*export const fetchUsers = createAsyncThunk(`/users/fetchUser`, async() => {
     const response = await client.get(`/fakeApi/users`)
     return response.data
-})
+})*/
 
 /*const usersSlice = createSlice({
     name: 'users',
@@ -31,11 +31,25 @@ export const fetchUsers = createAsyncThunk(`/users/fetchUser`, async() => {
     }
 })*/
 
+
+
+export const extendedApiSlice = apiSlice.injectEndpoints({
+    endpoints: builder => ({
+        getUsers: builder.query({
+            query: () => '/users'
+        })
+    })
+})
+
+
 // Calling `someEndpoint.select(someArg)` generates a new selector that will return
 // the query result object for a query with those parameters.
 // To generate a selector for a specific query argument, call `select(theQueryArg)`.
 // In this case, the users query has no params, so we don't pass anything to select()
-export const selectUsersResult = apiSlice.endpoints.getUsers.select()
+
+// export const selectUsersResult = apiSlice.endpoints.getUsers.select()
+export const selectUsersResult = extendedApiSlice.endpoints.getUsers.select()
+
 const emptyUsers = []
 
 export const selectAllUsers = createSelector(
@@ -54,3 +68,4 @@ export const selectUserById = createSelector(
 //export const selectAllUsers = state => state.users
 //export const selectUserById = (state, userId) => state.users.find(user => user.id === userId)
 //export const { selectAll: selectAllUsers, selectById: selectUserById } = usersAdapter.getSelectors(state => state.users)
+export const { useGetUsersQuery } = extendedApiSlice
